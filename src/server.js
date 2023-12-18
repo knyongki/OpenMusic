fffe9975d0a3ae87c3c4c091b4ef4e594c015776
knyongki <yyongkisetiawan@gmail.com>
2023-12-18 23:20:13 +0700

    authentications implements

--- src/server.js
+++ src/server.js
@@ -3,23 +3,31 @@ const Hapi = require('@hapi/hapi');
 
 // song
 const songs = require('./api/songs');
-const SongsService = require('./services/postgres/song_service');
+const SongsService = require('./services/postgres/songService');
 const SongsValidator = require('./validator/songs');
 
 // album
 const albums = require('./api/albums');
-const AlbumsService = require('./services/postgres/album_service');
+const AlbumsService = require('./services/postgres/albumService');
 const AlbumsValidator = require('./validator/albums');
 
 // users
 const users = require('./api/users');
-const UsersService = require('./services/postgres/user_service');
+const UsersService = require('./services/postgres/userService');
 const UsersValidator = require('./validator/users');
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/authenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 
 const init = async () => {
   const songService = new SongsService();
   const albumService = new AlbumsService();
   const userService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -49,6 +57,15 @@ const init = async () => {
       options: {
         service: userService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        userService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
